f3e28a67f48adb1adcede4e31baa5bb51f1c67f6
Sardar-Atabekov <43814348+Sardar-Atabekov@users.noreply.github.com>
2019-02-15 12:43:09 +0300

    Update script.js

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 	// Timer 
-	let deadline = '2019-02-09';
+	let deadline = '2019-03-09';
 
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
